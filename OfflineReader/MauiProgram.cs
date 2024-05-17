b463e8151e48066e4bdd9b8e3f3d6c25d21df509
EliorMigdal <eliormigdal7@gmail.com>
2024-05-17 16:04:30 +0300

    Kinda done with Main Page, moving to the next one;

--- OfflineReader/MauiProgram.cs
+++ OfflineReader/MauiProgram.cs
@@ -19,12 +19,6 @@ public static class MauiProgram
 				builder.Logging.AddDebug();
 		#endif
 
-		Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
-		builder.Services.AddSingleton<HomePage>();
-
-		Routing.RegisterRoute(nameof(FlyoutMenuPage), typeof(FlyoutMenuPage));
-		builder.Services.AddSingleton<FlyoutMenuPage>();
-
         Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
         builder.Services.AddSingleton<MainViewModel>();
         builder.Services.AddSingleton<MainPage>();
